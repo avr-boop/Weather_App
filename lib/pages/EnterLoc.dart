77aa9692da1f01c77464a3d914f3b28d6ddd6475
Varun Raj <varunrajdec@gmail.com>
2021-07-01 13:54:50 +0530

    finished initial route

--- lib/pages/EnterLoc.dart
+++ lib/pages/EnterLoc.dart
@@ -15,39 +15,61 @@ class _EnterLocState extends State<EnterLoc> {
         child: Scaffold(
             body: SingleChildScrollView(
                 child: Container(
+                    height: MediaQuery.of(context).size.height,
+                    width: MediaQuery.of(context).size.width,
                     decoration: BoxDecoration(
                         gradient: LinearGradient(
                             begin: Alignment.topCenter,
                             end: Alignment.bottomRight,
                             colors: [Colors.white, Colors.blue])),
-                    child: Column(
-                      children: [
-                        SizedBox(height: 250),
-                        Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: TextField(
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                              obscureText: false,
-                              controller: myController,
-                              decoration: InputDecoration(
-                                enabledBorder: OutlineInputBorder(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(22.0)),
-                                  borderSide: BorderSide(
-                                      color: Colors.black54, width: 4),
-                                ),
-                                focusedBorder: OutlineInputBorder(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(12.0)),
-                                  borderSide: BorderSide(color: Colors.red),
-                                ),
-                                labelText: "Enter your city",
-                                labelStyle: TextStyle(color: Colors.black87),
-                                hintText:
-                                    'Please enter City name without spelling error',
-                              )),
-                        ),
-                      ],
+                    child: Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 350, 0, 0),
+                      child: Column(
+                        children: [
+                          /*SizedBox(height: 250),*/
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: TextField(
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                                obscureText: false,
+                                controller: myController,
+                                decoration: InputDecoration(
+                                  enabledBorder: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(18.0)),
+                                    borderSide: BorderSide(
+                                        color: Colors.black54, width: 4),
+                                  ),
+                                  focusedBorder: OutlineInputBorder(
+                                    borderRadius:
+                                        BorderRadius.all(Radius.circular(18.0)),
+                                    borderSide: BorderSide(
+                                        color: Colors.deepOrange.shade300,
+                                        width: 3),
+                                  ),
+                                  labelText: "Enter your city",
+                                  labelStyle: TextStyle(color: Colors.black87),
+                                  hintText: 'Please enter City name',
+                                )),
+                          ),
+                          /*SizedBox(
+                            height: 30,
+                          ),*/
+                          ElevatedButton(
+                            onPressed: () {
+                              print("hi");
+                            },
+                            child: Text("Check Weather"),
+                            style: ElevatedButton.styleFrom(
+                              onPrimary: Colors.white,
+                              elevation: 20.0,
+                              shadowColor: Colors.blueAccent,
+                              onSurface: Colors.grey,
+                              primary: Colors.grey.shade800,
+                            ),
+                          ),
+                        ],
+                      ),
                     )))));
   }
 }
